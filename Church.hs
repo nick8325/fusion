fce80541ae4610949b57b500cff9b7bb083ab294
Nick Smallbone <nick.smallbone@gmail.com>
2012-08-13 13:06:01 +0100

    church-encoding works for foldl!

--- Church.hs
+++ Church.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE Rank2Types #-}
 
-module Church(test) where
+module Church(test, test2, test3, foldl2) where
 
-import Prelude(undefined, ($))
+import Prelude hiding (map, (++), foldl, sum)
 
 newtype List a = List { fold :: forall b. (a -> b -> b) -> b -> b }
 
@@ -15,6 +15,40 @@ cons x xs = List $ \c n -> c x (fold xs c n)
 map :: (a -> b) -> List a -> List b
 map f xs = List $ \c n -> fold xs (\x xs -> f x `c` xs) n
 
+{-# INLINE (++) #-}
+(++) :: List a -> List a -> List a
+xs ++ ys = List $ \c n -> fold xs c (fold ys c n)
+
+{-# INLINE sum #-}
+sum :: List Int -> Int
+sum = foldl (+) 0
+
+-- Look ma, no space leak!
+-- And with foldl' there's still no space leak! Honest!
+{-# INLINE foldl #-}
+foldl :: (a -> b -> a) -> a -> List b -> a
+foldl op e xs = fold xs (\x y -> y . (`op` x)) id e
+
+{-# INLINE toCh #-}
+toCh xs = List $ \op e -> myFoldr op e xs
+
+{-# INLINE myFoldr #-}
+myFoldr op e = aux
+  where aux [] = e
+        aux (x:xs) = x `op` aux xs
+
+{-# INLINE fromCh #-}
+fromCh l = fold l (:) []
+
 {-# NOINLINE test #-}
-test :: (a -> b) -> (b -> c) -> List a -> List c
-test f g xs = map g (map f xs)
+test :: (a -> b) -> (b -> c) -> [a] -> [c]
+test f g xs = fromCh (map g (map f (toCh xs)))
+
+{-# NOINLINE test2 #-}
+test2 :: [Int] -> [Int] -> Int
+test2 xs ys = sum (toCh xs ++ toCh ys)
+
+{-# NOINLINE test3 #-}
+test3 :: [Int] -> Int
+test3 xs = sum (map square (toCh xs))
+  where square x = x * x
--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 all: Church.hcr
 
 %.hcr: %.hs
-	ghc -c -O -ddump-simpl $< > $@
+	ghc -c -O -ddump-simpl -dsuppress-module-prefixes -dsuppress-uniques -dsuppress-coercions $< > $@
